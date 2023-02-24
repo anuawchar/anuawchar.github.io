59283ef2786be0734308608f6e866ed9e7b334cf
anuawchar <anujaawchar@gmail.com>
2023-02-24 14:46:16 +0530

    Beautiful Girl

--- ShopSite/admin_area/index.php
+++ ShopSite/admin_area/index.php
@@ -87,12 +87,12 @@
             ?>
         </div>
 
-         <!-- last child -->
+         <!-- last child
 <div class="bg-info p-3 text-center footer">
     <p>All rights reserved @- Designed by GPK Student-2023</p>
 </div>
     </div>
-
+ -->
  <!-- bootstrap js link -->
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
